eb086ac5553f11f06e9eec07cf0b0e0be685f4d7
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-10-04 01:33:24 +0600

    image and video download implemented

--- scraper.py
+++ scraper.py
@@ -4,6 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from utils import random_delay, infinite_scroll
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import os
+import io
+from PIL import Image
+import yt_dlp
 
 import undetected_chromedriver as uc
 # **** Note: Using selenium webdriver was invoking CAPTCHA. Using undetected_chromedrver solved the issue ***** 
@@ -80,6 +87,7 @@ def scrape_profile_video_urls(username):
 
     driver.quit()
 
+
 def scrape_profile_photo_urls(username):
     """
     takes the Tiktok username of a profile as input and retrieves the Tiktok urls of all the photo post of that profile. returns the video urls in a list.
@@ -107,6 +115,7 @@ def scrape_profile_photo_urls(username):
     random_delay()
 
     driver.quit()
+
 
 def scrape_single_video_data(url: str):
 
@@ -150,4 +159,71 @@ def scrape_single_video_data(url: str):
 
     driver.quit()
 
-scrape_profile_photo_urls("aura.attires")
+
+## works well for all public videos
+def download_video(video_url, save_path='tiktok_videos'):
+    """
+    Provide the link of the video post and it will download it in the path.
+    """
+    # Ensure the save directory exists
+    if not os.path.exists(save_path):
+        os.makedirs(save_path) # this creates nested directories, too
+
+    # Configure yt-dlp options
+    ydl_opts = {
+        'outtmpl': os.path.join(save_path, '%(id)s.%(ext)s'), # instead of Id, title can be put as well
+        'format': 'best',
+    }
+
+    try:
+        # Create a yt-dlp object and download the video
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(video_url, download=True)
+            filename = ydl.prepare_filename(info)
+            print(f"Video successfully downloaded: {filename}")
+    
+    except Exception as e:
+        print(f"Error downloading video: {str(e)}")
+
+
+## This function works for Tiktok, Insta, Medium, LinkedIn, FB. Worked for jpg, webp. Didn't work for avif format.
+def download_image(download_path, url, file_name):
+    """
+        takes download_path (folder path), url (image url) and file_name (new image file name with .jpg extension)
+
+        saves the image in the path.
+    """
+    ### Works without adding header. In case of any issue, try adding header
+    # headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
+    #     "Referer": "https://www.tiktok.com/",
+    #     "Accept-Language": "en-US,en;q=0.5",
+    # }
+
+    try:
+        response = requests.get(url=url)
+        
+        if response.status_code != 200:
+            print(f"Failed to retrieve image. Status code: {response.status_code}")
+            return
+        
+        # Check the Content-Type header
+        content_type = response.headers.get('Content-Type')
+        print(content_type)
+        if 'image' not in content_type:
+            print(f"Unexpected content type: {content_type}")
+            return
+        
+        image_file = io.BytesIO(response.content)
+        image = Image.open(image_file)
+        file_path = download_path + file_name
+
+        with open(file_path, "wb") as f:
+            image.save(f, "JPEG")
+
+        print("Success")
+    except Exception as e:
+        print("Error:", e)
+
+
+download_video("https://www.tiktok.com/@d1asif/video/7179462609714810114?q=d1asif&t=1727966362497")
